919b85105a18d53e1ea719fcc547f1032b5217b9
okorkov <alex.okorkov@gmail.com>
2020-11-11 17:00:34 -0800

    commit tree 5331ab952cfd677801cda14b939b7125440fbc79

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) 
+  count = 0
+  while count < array.length
+    yield array[count]
+    count += 1
+  end
+  array
 end
+
+collection = [1, 2, 3, 4, 5, 6, 7]
+my_each(collection) { |i| print i}
